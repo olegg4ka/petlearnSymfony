71c32b6c2f0472068386f7bcf5abf4dbe783f1f1
olegg4ka <oleg2934@gmail.com>
2021-03-16 19:02:25 +0200

    COURS-4 шаблоны

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,21 +9,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-    	$temp = 123;
-        return $this->render('default/index.html.twig', ['temp' => $temp]);
-    }
+	/**
+	 * @Route("/", name="homepage")
+	 */
+	public function indexAction(Request $request)
+	{
+		$temp = 123;
+		$array = [1, 2, 3];
+		$bool = false;
+		return $this->render('default/index.html.twig', ['temp' => $temp, 'array' => $array, 'bool' => $bool]);
+	}
 
 	/**
 	 * @Route("/feedback", name="feedback")
 	 */
-    public function feedbackAction(){
-    	//обезательно use Symfony\Component\HttpFoundation\Response;
-	    return $this->render('default/feedback.html.twig');
-    }
+	public function feedbackAction()
+	{
+		//обезательно use Symfony\Component\HttpFoundation\Response;
+		return $this->render('default/feedback.html.twig');
+	}
 
 }
